a7a040c1ca095140c6eb00a2dfed637e4086d5a4
Anton Prokopev <nbb1@bk.ru>
2022-04-14 21:25:12 +0300

    sentMessage

--- Chat/Controllers/MainControllers/ChatsViewController.swift
+++ Chat/Controllers/MainControllers/ChatsViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 import MessageKit
+import InputBarAccessoryView
 
 class ChatsViewController: MessagesViewController {
+
+	
 	
 	//MARK: - Properties
 	private var messages: [MMessage] = []  //необходимо подписать вашу модель под протокол MessageType
@@ -32,9 +35,25 @@ class ChatsViewController: MessagesViewController {
 	//MARK: - Lifecycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		if let layout = messagesCollectionView.collectionViewLayout as? MessagesCollectionViewFlowLayout {
+			layout.textMessageSizeCalculator.outgoingAvatarSize = .zero 
+		}
+		messagesCollectionView.backgroundColor = .mainWhite()
 		configureMessageInputBar()
-		configureSendButton()
+		messageInputBar.delegate = self
+		messagesCollectionView.messagesDataSource = self
+		messagesCollectionView.messagesLayoutDelegate = self
+		messagesCollectionView.messagesDisplayDelegate = self
+		
 	}
+	
+	private func insertNewMessage(message: MMessage) {
+		guard !messages.contains(message) else { return } //проверка на отсутствие в нашем текущем массиве новых сообщений
+		messages.append(message)
+		messages.sort()
+		messagesCollectionView.reloadData()
+	}
+	
 }
 
 
@@ -61,39 +80,92 @@ extension ChatsViewController: MessagesDataSource {
 }
 
 
+//MARK: - MessagesLayoutDelegate
+extension ChatsViewController: MessagesLayoutDelegate {
+	func footerViewSize(for section: Int, in messagesCollectionView: MessagesCollectionView) -> CGSize {
+		return CGSize(width: 0, height: 8)
+	}
+}
+
+extension ChatsViewController: MessagesDisplayDelegate {
+	//красим фон сообщения
+	func backgroundColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
+		return isFromCurrentSender(message: message) ? .white : #colorLiteral(red: 0.8137198091, green: 0.7213935256, blue: 0.953623116, alpha: 1) // подкапотная булевая функция определяющая кто отправил сообщение.
+	}
+	
+	//красим текст сообщения
+	func textColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
+		return isFromCurrentSender(message: message) ? #colorLiteral(red: 0.2392156863, green: 0.2392156863, blue: 0.2392156863, alpha: 1) : .white // подкапотная булевая функция определяющая кто отправил сообщение.
+	}
+	
+	//настраиваем отоброжение аватарок в чате
+	func configureAvatarView(_ avatarView: AvatarView, for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) {
+		isFromCurrentSender(message: message) ? (avatarView.isHidden = true) : (avatarView.isHidden = false)
+	}
+	
+	//настраиваем стиль сообщений, в MessageStyle можно посмотреть различные кейсы
+	func messageStyle(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageStyle {
+		return .bubble
+	}
+	
+		//после необходимых настроек создаем приватную функцию ниже viewDidLoad для отображений сообщений на экране "insertNewMessage"
+}
+
+extension ChatsViewController: InputBarAccessoryViewDelegate {
+	func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+		let message = MMessage(user: user, content: text)
+		insertNewMessage(message: message)
+		inputBar.inputTextView.text = ""
+	}
+	
+//	func inputBar(_ inputBar: InputBarAccessoryView, didChangeIntrinsicContentTo size: CGSize) {
+//		<#code#>
+//	}
+//
+//	func inputBar(_ inputBar: InputBarAccessoryView, textViewTextDidChangeTo text: String) {
+//		<#code#>
+//	}
+//
+//	func inputBar(_ inputBar: InputBarAccessoryView, didSwipeTextViewWith gesture: UISwipeGestureRecognizer) {
+//		<#code#>
+//	}
+}
+
+
+
 // MARK: - ConfigureMessageInputBar
 //делаем кастомный текстфилд инструментами библиотеки MessageKit
 extension ChatsViewController {
-		func configureMessageInputBar() {
-				messageInputBar.isTranslucent = true
-				messageInputBar.separatorLine.isHidden = true
-				messageInputBar.backgroundView.backgroundColor = .mainWhite()
-				messageInputBar.inputTextView.backgroundColor = .white
-				messageInputBar.inputTextView.placeholderTextColor = #colorLiteral(red: 0.7411764706, green: 0.7411764706, blue: 0.7411764706, alpha: 1)
-				messageInputBar.inputTextView.textContainerInset = UIEdgeInsets(top: 14, left: 30, bottom: 14, right: 36)
-				messageInputBar.inputTextView.placeholderLabelInsets = UIEdgeInsets(top: 14, left: 36, bottom: 14, right: 36)
-			  messageInputBar.inputTextView.placeholderLabel.text = "Message"
-				messageInputBar.inputTextView.layer.borderColor = #colorLiteral(red: 0.7411764706, green: 0.7411764706, blue: 0.7411764706, alpha: 0.4033635232)
-				messageInputBar.inputTextView.layer.borderWidth = 0.2
-				messageInputBar.inputTextView.layer.cornerRadius = 18.0
-				messageInputBar.inputTextView.layer.masksToBounds = true
-				messageInputBar.inputTextView.scrollIndicatorInsets = UIEdgeInsets(top: 14, left: 0, bottom: 14, right: 0)
-				
-				
-				messageInputBar.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-				messageInputBar.layer.shadowRadius = 5
-				messageInputBar.layer.shadowOpacity = 0.3
-				messageInputBar.layer.shadowOffset = CGSize(width: 0, height: 4)
-				
-				configureSendButton()
-		}
+	func configureSendButton() {
+		messageInputBar.sendButton.setImage(UIImage(named: "Sent"), for: .normal)
+		messageInputBar.sendButton.applyGradients(cornerRadius: 10)
+		messageInputBar.setRightStackViewWidthConstant(to: 56, animated: false)
+		messageInputBar.sendButton.contentEdgeInsets = UIEdgeInsets(top: 2, left: 2, bottom: 6, right: 30)
+		messageInputBar.sendButton.setSize(CGSize(width: 48, height: 48), animated: false)
+		messageInputBar.middleContentViewPadding.right = -38
+	}
+	
+	func configureMessageInputBar() {
+		messageInputBar.isTranslucent = true
+		messageInputBar.separatorLine.isHidden = true
+		messageInputBar.backgroundView.backgroundColor = .mainWhite()
+		messageInputBar.inputTextView.backgroundColor = .white
+		messageInputBar.inputTextView.placeholderTextColor = #colorLiteral(red: 0.7411764706, green: 0.7411764706, blue: 0.7411764706, alpha: 1)
+		messageInputBar.inputTextView.textContainerInset = UIEdgeInsets(top: 14, left: 30, bottom: 14, right: 36)
+		messageInputBar.inputTextView.placeholderLabelInsets = UIEdgeInsets(top: 14, left: 36, bottom: 14, right: 36)
+		messageInputBar.inputTextView.placeholderLabel.text = "Message"
+		messageInputBar.inputTextView.layer.borderColor = #colorLiteral(red: 0.7411764706, green: 0.7411764706, blue: 0.7411764706, alpha: 0.4033635232)
+		messageInputBar.inputTextView.layer.borderWidth = 0.2
+		messageInputBar.inputTextView.layer.cornerRadius = 18.0
+		messageInputBar.inputTextView.layer.masksToBounds = true
+		messageInputBar.inputTextView.scrollIndicatorInsets = UIEdgeInsets(top: 14, left: 0, bottom: 14, right: 0)
 		
-		func configureSendButton() {
-				messageInputBar.sendButton.setImage(UIImage(named: "Sent"), for: .normal)
-				messageInputBar.sendButton.applyGradients(cornerRadius: 10)
-				messageInputBar.setRightStackViewWidthConstant(to: 56, animated: false)
-				messageInputBar.sendButton.contentEdgeInsets = UIEdgeInsets(top: 2, left: 2, bottom: 6, right: 30)
-				messageInputBar.sendButton.setSize(CGSize(width: 48, height: 48), animated: false)
-				messageInputBar.middleContentViewPadding.right = -38
-		}
+		
+		messageInputBar.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+		messageInputBar.layer.shadowRadius = 5
+		messageInputBar.layer.shadowOpacity = 0.3
+		messageInputBar.layer.shadowOffset = CGSize(width: 0, height: 4)
+		
+		configureSendButton()
+	}
 }
